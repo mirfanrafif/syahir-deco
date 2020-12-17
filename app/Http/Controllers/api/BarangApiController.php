66e3d1fc5b0f25feb51be9cb877ac0372d4aa339
Irfan Rafif <mirfanrafif17@gmail.com>
2020-12-17 13:10:57 +0700

    cek ketersediaan selesai

--- app/Http/Controllers/api/BarangApiController.php
+++ app/Http/Controllers/api/BarangApiController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Barang;
+use App\Models\Sewa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BarangApiController extends Controller
 {
     public function cekKetersediaan(Request $request)
     {
-        
+        $barang = Barang::find($request->id);
+        $tanggalSewa = $request->cektanggal;
+
+        // return $tanggalSewa;
+        $sewa = $barang->sewa->where('tanggal_sewa', $tanggalSewa)->count();
+
+        if ($sewa < $barang->stok) {
+            $data = [
+                "status" => "ADA",
+                "sewa" => $sewa
+            ];
+            return response()->json($data);
+        } else {
+            $data = [
+                "status" => "KOSONG",
+                "sewa" => $sewa
+            ];
+            return response()->json($data);
+        }
     }
 }
--- resources/views/detailbarang.blade.php
+++ resources/views/detailbarang.blade.php
@@ -1,7 +1,4 @@
-@extends('templates.home')
-
-@section('title', 'Detail Barang')
-
+@extends('templates.home') @section('title', 'Detail Barang')
 @section('content')
 <section class="section bg-gray">
   <!-- Container Start -->
@@ -13,20 +10,22 @@
           <h1 class="product-title">{{$barang->jenis_paket}}</h1>
           <div class="product-meta">
             <ul class="list-inline">
-              <li class="list-inline-item"><i class="fa fa-folder-open-o"></i> Kategori<a
-                  href="">{{$barang->kategori}}</a></li>
-              <li class="list-inline-item"><i class="fa fa-location-arrow"></i> Kategori Acara<a
-                  href="">{{$barang->kategori_acara}}</a>
+              <li class="list-inline-item">
+                <i class="fa fa-folder-open-o"></i> Kategori<a href="">{{$barang->kategori}}</a>
+              </li>
+              <li class="list-inline-item">
+                <i class="fa fa-location-arrow"></i> Kategori
+                Acara<a href="">{{$barang->kategori_acara}}</a>
               </li>
             </ul>
           </div>
 
           <!-- product slider -->
-          <img class="d-block img-fluid w-100" src="{{asset('images/products/products-1.jpg')}}" alt="product-img">
+          <img class="d-block img-fluid w-100" src="{{ asset('images/products/products-1.jpg') }}" alt="product-img" />
           <!-- product slider -->
 
           <div class="content mt-5 pt-5">
-            <ul class="nav nav-pills  justify-content-center" id="pills-tab" role="tablist">
+            <ul class="nav nav-pills justify-content-center" id="pills-tab" role="tablist">
               <li class="nav-item">
                 <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
                   aria-controls="pills-home" aria-selected="true">Product Details</a>
@@ -51,39 +50,49 @@
           <!-- Safety tips widget -->
           <div class="widget disclaimer">
             <h5 class="widget-header">Cek Ketersediaan</h5>
-            <form action="">
+            <form action="" method="dialog">
               <div class="form-group">
                 <label for="cektanggal">Tanggal</label>
                 <input type="date" name="cektanggal" id="cektanggal" class="form-control" placeholder=""
-                  aria-describedby="helpId">
+                  aria-describedby="helpId" />
               </div>
-              <input type="submit" value="Cek" class="btn btn-primary">
+              <input type="submit" value="Cek" class="btn btn-primary" id="cekKetersediaan" />
+              <h1 id="status" style="text-align: center"></h1>
             </form>
           </div>
           <!-- Coupon Widget -->
           <div class="widget coupon text-center">
             <!-- Coupon description -->
-            <p>Tertarik dengan Produk Kami?
-            </p>
+            <p>Tertarik dengan Produk Kami?</p>
             <!-- Submii button -->
             <a href="/sewa" class="btn btn-transparent-white">Pesan Sekarang</a>
           </div>
-
         </div>
       </div>
-
     </div>
   </div>
 
   <script>
     $(document).ready(function () {
-      var formatter = new Intl.NumberFormat('de-DE');
-      
-      var harga = $("#harga").text();
-      var hargaConverted = formatter.format(harga);
-      console.log(hargaConverted);
-      $("#harga").text("Rp. " + hargaConverted);
-    });
+          var formatter = new Intl.NumberFormat('de-DE');
+
+          var harga = $("#harga").text();
+          var hargaConverted = formatter.format(harga);
+          console.log(hargaConverted);
+          $("#harga").text("Rp. " + hargaConverted);
+
+          $("#cekKetersediaan").click(function () {
+            {
+            var cektanggal = $("#cektanggal").val();
+
+            console.log(cektanggal);
+            $.get("/api/sewa/" + {{$barang->idbarang}}, {"cektanggal" : cektanggal}).done(function (data) {
+              console.log(data)
+              $("#status").text(data.status);
+            })
+          }
+          })
+        });
   </script>
   <!-- Container End -->
 </section>
